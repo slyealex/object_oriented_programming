3d655eb60a384bdcea652cb2e11a781654a66684
ALEX SLYE <slyealex@live.com>
2016-01-14 14:23:29 -0500

    added Rover class and required methods for state and behavior

---
+++ mars_rover.rb
@@ -0,0 +1,21 @@
+class Rover
+
+  def initialize(x_coordinate, y_coordinate, direction)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @direction = direction
+  end
+
+  def read_instruction
+
+  end
+
+  def move
+
+  end
+
+  def turn
+
+  end
+
+end

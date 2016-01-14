6c4685c8cecea32507c5a2e20ba6c979f7a77932
ALEX SLYE <slyealex@live.com>
2016-01-14 13:20:48 -0500

    Completed exercise 1

--- people.rb
+++ people.rb
@@ -25,3 +25,18 @@ end
 
 chris = Instructor.new("chris")
  p chris.greeting
+
+cristina = Student.new("cristina")
+p cristina.greeting
+
+p chris.teach
+p cristina.learn
+
+# chris.learn
+# cristina.teach
+
+# The above will give an error, this is becuase chris is an instance
+# of the Instructor class and cristina is an instance of the Student class
+# If you try to call a method on an instance of another class, you will
+# get an error saying undefined local variable as they are not able to access
+# the methods of another class unless it is inherited from a parent.

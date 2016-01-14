2abf927596ab9d23aefa0008443272027db88c0e
ALEX SLYE <slyealex@live.com>
2016-01-14 17:05:59 -0500

    started work on rover, made grid prompts

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,4 +1,6 @@
+
 class Rover
+  attr_accessor :x_coordinate, :y_coordinate, :direction
 
   def initialize(x_coordinate, y_coordinate, direction)
     @x_coordinate = x_coordinate
@@ -7,15 +9,43 @@ class Rover
   end
 
   def read_instruction
+    if turn
 
   end
 
   def move
-
+    if @direction == "N"
+      @y_coordinate += 1
+    elsif @direction == "S"
+      @y_coordinate -= 1
+    elsif @direction == "W"
+      @x_coordinate -= 1
+    else @direction == "E"
+      @x_coordinate += 1
+    end
   end
 
   def turn
-
+    if l
+      @direction =
+    elsif r
+      @direction =
+    end
   end
 
 end
+
+rover1 = Rover.new()
+
+p "plataeu size"
+plateau = gets.chomp.split
+p "Where is the Rover?"
+rover_init = gets.chomp.split
+
+@x_coordinate = rover_init[0].to_i
+@y_coordinate = rover_init[1].to_i
+@direction = rover_init[2]
+puts "#{@x_coordinate}, #{@y_coordinate}, #{@direction}"
+
+x_max == plateau[0].to_i
+y_max == plateau[1].to_i
